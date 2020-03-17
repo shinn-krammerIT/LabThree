8824f2298d68270bc9675d9aba08176891c703c3
[Shinn Krammer] <[aa9e2d379a5e29cc15ac5aa7953e8ca0c32ac299]>
2020-03-17 21:50:35 +1300

    Finished journal

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# LabThree
+# LabThree Journal
+Shinn Krammer
+03/03/20
+
+What have I learnt?
+This week I have learnt about if and switch statements and how to apply them in javascript code. 
+
+Why have I learnt this?
+I have learnt this so I can use if and swtich staements in javascript. If and switch statements are very well used JS statements and are crucial for adavanced JS scripts. 
+
+How have I learnt this?
+I have learnt this by reading information about these JS statements in the provided gitbook. I have also created the code in lab 3 to specification. 
+
+What do I know now that I didn't know before?
+I did not know about the switch statement before this week. I feel it is very clean looking compared to if and easier to use for specific strings/values. 
+
+
+What new understanding/skills have you gained?
+I have gained skills in applying switch statements. I feel that having knowledge of this statement earlier on in my learning of coding would have been very useful since they are easier to use with specific strings and values. 
+
+What are the real life/work applications?
+Since these statements are really fundemental to general coding I feel that I will be using these in all aspects of a coding career. 
--- index.js
+++ index.js
@@ -11,42 +11,42 @@ let desc = "blank"
 
 //if statement used for grade
 if (mark == 100) { // if mark is 100/100
-    grade = " an A+. ";
+    grade = " an A+. "; // A+
     gradeDesc = "Perfect";
 } else if (mark < 100 && mark >= 75) { // if mark is 75 - 100
     gradeDesc = "Pass with Distinction"; 
     if (mark < 100 && mark >= 85) { // nest if: if mark is 85 - 100 
-        grade = " an A+. ";
+        grade = " an A+. "; // A+
     } else if (mark < 85 && mark >= 80) { // nest if: if mark is 80 - 85
-        grade = " an A. ";
+        grade = " an A. "; // A
     } else if (mark < 80 && mark >= 75) { // nest if: if mark is 75 - 80 
-        grade = " an A-. ";
+        grade = " an A-. "; // A-
     }
 } else if (mark < 75 && mark >= 60) { // if mark is 60 - 75
     gradeDesc = "Pass with Merit";
     if (mark < 75 && mark >= 70) { // nest if: if mark is 70 - 75
-        grade = " a B+. ";
+        grade = " a B+. "; // B+
     } else if (mark < 70 && mark >= 65) { // nest if: if mark is 65 - 70
-        grade = " a B. ";
+        grade = " a B. "; // B
     } else if (mark < 65 && mark >= 60) { // nest if: if mark is 60 - 65
-        grade = " a B-. ";
+        grade = " a B-. "; // B-
     }
 } else if (mark < 60 && mark >= 50) { // if mark is 50 - 60
     gradeDesc = "Pass";
     if (mark < 60 && mark >= 55) { // nest if: if mark is 55 - 60
-        grade = " a C+. ";
+        grade = " a C+. "; // C+
     } else if (mark < 55 && mark >= 50){ // nest if: if mark is 50 - 55
-        grade = " a C. ";
+        grade = " a C. "; // C
     }
 } else if (mark < 50 && mark > 0) { // if mark is above 0 - 50
-    gradeDesc = "Fail Grades";
+    gradeDesc = "Fail Grades"; 
     if (mark < 50 && mark >= 40) { // nest if: if mark is 40 - 50
-        grade = " a D. ";
+        grade = " a D. "; // D
     } else if (mark < 40 && mark > 0) { // nest if: if mark is 0 - 40
-        grade = " an E. ";
+        grade = " an E. "; // E
     }
 } else if (mark == 0) { // if mark is 0
-    grade = " an F. ";
+    grade = " an F. "; // F
     gradeDesc = "Complete Fail";
 } else if ((mark < 0 || mark > 100) || mark == NaN) { // Invalid input check. If mark is not a number or is less than zero or greater than 100
     console.log("Please input a valid grade.");
